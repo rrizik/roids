0add6cdf8abf3bc8678116f5d4a1c89f1b5ca8aa
rafik rizik <rizik944@gmail.com>
2023-11-09 03:07:39 -0600

    cleaned up shader

--- data/shaders/simple_shader.hlsl
+++ data/shaders/simple_shader.hlsl
@@ -20,40 +20,13 @@ struct VS_Output{
 VS_Output vs_main(InputLayout input){
     VS_Output output;
 
-    //float4x4 pv = mul(view, projection);
-    //float4x4 pvm = mul(pv, input.model_transform);
-    //float4 result = mul(pvm, float4(input.vertex_position, 1));
-    //output.pos = result;
 
-    // (((IN*M)*V)*P)
-    //float4 world_position = mul(input.model_transform, float4(input.vertex_position, 1.0));
-    //float4 view_position = mul(view, world_position);
-    //float4 result = mul(projection, world_position);
-    // (((P*V)*M)*IN)
-    //float4x4 pv = mul(projection, view);
-    //float4x4 pvm = mul(pv, input.model_transform);
-    //float4 result = mul(pv, float4(input.vertex_position, 1));
-
-    float4 world_space = mul(input.model_transform, float4(input.vertex_position, 1));
+    // todo(rr): verify variable names are correct. What is result?
+    float4 world_space = mul(float4(input.vertex_position, 1), input.model_transform);
     float4 camera_space = mul(world_space, view);
-    //float4 result = mul(camera_space, projection);
-    float4 result = mul(float4(input.vertex_position, 1), mul(input.model_transform, mul(view, projection)));
+    float4 result = mul(camera_space, projection);
     output.pos = result;
 
-    // (((IN*M)*V)*P)
-    //float4 inm = mul(float4(input.vertex_position, 1), input.model_transform);
-    //float4 inmv = mul(inm, view);
-    //float4 result = mul(inmv, projection);
-    //output.pos = result;
-    // camera multiplication
-    //float4 world_position = mul(float4(input.vertex_position, 1), input.model_transform);
-    //float4 view_position = mul(view, world_position);
-    //output.pos = mul(projection, view_position);
-
-    //output.pos = mul(mul(float4(input.vertex_position, 1.0f), input.transform), transform);
-    //output.pos = mul(float4(input.vertex_position, 1.0f), mul(input.transform, transform));
-    //output.pos = mul(float4(input.vertex_position, 1.0f), input.transform);
-    //output.pos = mul(float4(input.vertex_position, 1.0f), transform);
     output.tex = input.tex;
     return output;
 }
